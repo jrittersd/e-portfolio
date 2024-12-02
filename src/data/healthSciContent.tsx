848b409936ef45501a0bbb243784b96d2bf08140
afsharritter <jamesafsharritter@gmail.com>
2024-12-02 00:27:18 -0800

    finished health sci

--- src/data/healthSciContent.tsx
+++ src/data/healthSciContent.tsx
@@ -1,20 +1,22 @@
 export const artifacts = [
   {
-    name: "Artifact 1",
-    text: `    This is my first artifact.`,
-    link: "/e-portfolio/assets/usd.png",
+    name: "Annotated Timeline of Legislation: Health Information Technology",
+    text: `    This artifact, an Annotated Timeline of Legislation, was one of the first assignments I completed in the MSHCI program and allowed me to delve into the history of Health Information Technology (HIT) in the United States. While brief, this artifact details some of the milestones in the evolution of Healthcare Informatics, from its inception under the Kennedy administration through the creation of the Office of the National Coordinator for Health Information Technology in the early 2000s and the subsequent legislation (i.e., the HITECH Act and MACRA) that further expanded and incentivized its use. 
+        A recurring theme in this history is the dynamic interplay between providers, regulatory organizations, and HIT vendors. Vendors often drive the industry forward with new innovations, yet providers often resist adopting these technologies due to legitimate concerns such as cost, usability, workflow disruption, and patient safety/satisfaction. This compels legislators and regulatory bodies to strike a delicate balance between incentivization and regulation, encouraging providers to adopt new technologies while placing significant oversight on vendors. This history underscores the collaborative balance that has shaped the HIT industry.`,
+    link: "/e-portfolio/assets/healthSci/HealthSci_AnnotatedTimeline_541.pdf",
   },
   {
-    name: "Preventative Care & Screening for Depression - Project Overview",
-    text: `    My Preventative Care & Screening for Depression Project Overview satisfies the MSHCI Program Objective of Health Science Knowledge & Skills by exemplifying my understanding of value-based care in the context of population health, which often aims to design specific measures to improve patient outcomes in a community. This project was completed for HCIN 600: Population Health Analytics and proposes the development of registries and clinical decision support tools to facilitate screening for depression in primary care settings. 
-        The goal of these interventions is to improve access to preventative mental health services, especially in marginalized communities. This artifact details the Introduction and Overview sections of the proposal, which delve into the Situation/Background/Assessment/Recommendation, Justifications, Interventions, and Cost Analysis for this project. This project helped me to solidify my understanding of population health analytics and value-based care as a framework for improving patient outcomes in various communities. `,
-    link: "/e-portfolio/assets/HealthSci_PreventativeCare_Overview.pdf",
+    name: "The Geography of Diabetes",
+    text: `    This artifact, The Geography of Diabetes, demonstrates my understanding of the drivers of chronic disease and the roles that organizations play in developing targeted and effective interventions. Using publically available data from the Health Resources and Services Administration (HRSA) visualized in Tableau, I identified an increased prevalence of Diabetes Mellitus (DM) in the lower latitudes of the United States, particularly in the American South and the Pacific Islands. This disparity is driven by a combination of cultural, socioeconomic, and systemic factors that reduce access to care and increase disease burden.
+        My analysis emphasized the importance of targeted, evidence-based interventions, like North Carolina's Diabetes Prevention Program, which was a state-funded effort to increase screenings, patient education, and access to support services in especially marginalized communities. This project highlights the geographic and systemic factors often fuel chronic disease in the United States and underscores the value data analytics brings in driving equitable healthcare policy. `,
+    link: "/e-portfolio/assets/healthSci/HealthSci_Diabetes_547.pdf",
   },
   {
-    name: "TBD: Augmenting Mental Health Treatment with AI Chatbots",
-    text: `    This evidence-based research project satisfies the MSHCI Program Outcome of Health Science Knowledge & Skills by exemplifying my understanding of clinical research and biostatistics. This project proposes a randomized clinical trial with a quantitative approach to evaluate the effectiveness of a commercially available AI-based chatbot to supplement traditional mental health services in adolescents. ~~~TBD~~~
-    Assignment is due December 16th and the final paper has not been submitted yet.`,
-    link: "/e-portfolio/assets/HealthSci_HCIN615_PLACEHOLDER.pdf",
+    name: "Preventative Care & Screening for Depression - Introduction",
+    text: `    This artifact, the introductory sections of my Preventative Care & Screening for Depression: Project Proposal presentation, demonstrates my understanding of value-based care-delivery models and the critical role providers and organizations play in improving population health. By proposing a value-based care intervention that comprises a registry with clinical decision support, this project highlights how PCPs can leverage their frequent patient interactions to identify and address depression in its earliest stages when treatment is more cost-effective and leads to improved patient outcomes. 
+        The PHQ-9-based clinical decision support tool would standardize the detection of depression through an evidence-based, highly effective screening method. This tool would work in tandem with the proposed registry, which tracks patient screenings and flags individuals needing follow-up care. Combined, these interventions would allow PCPs to provide proactive and coordinated care, reducing costs and improving patient outcomes, ultimately supporting the goals of value-based care.
+            This project underscores the integrative nature of value-based care, where thoughtfully designed health information technology can optimize care delivery and improve outcomes. It reflects my ability to connect the theoretical underpinnings of value-based care with the practical application of clinical support technology to develop innovative solutions that address gaps in the healthcare delivery system. `,
+    link: "/e-portfolio/assets/healthSci/HealthSci_PreventativeCare_600.pdf",
   },
 ];
 
