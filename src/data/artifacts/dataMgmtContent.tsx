9a2f88c1a88ac6394be14cbb9ab299b2c1f2605b
afsharritter <jamesafsharritter@gmail.com>
2024-12-14 17:00:17 -0800

    adding 1 data artifact

--- src/data/artifacts/dataMgmtContent.tsx
+++ src/data/artifacts/dataMgmtContent.tsx
@@ -6,7 +6,8 @@ export const artifacts = [
   },
   {
     name: "Youth Risk Behavior Surveilance System Database",
-    text: `    Reflection in dev.`,
+    text: `    This artifact, titled "Youth Risk Behavior Surveillance System," was my final project for HCIN 543: Database Design and Knowledge Management. This project demonstrates my understanding of data collection and analytics, data management software, and knowledge management theory pertaining to the program outcome of Data and Knowledge Management. In this project, a publically available data source, the YRBSS, was obtained and imported using Microsoft Access, where complex queries aimed to relate various demographic and societal features to participant outcomes.
+        This presentation provides a comprehensive overview of the data analysis and knowledge management process, detailing the data collection methodologies and feature definitions for the data source, building the database in MS Access, writing SQL queries, and generating data visualizations in MS Excel. The outputs of this process enabled a complex analysis of the various risk factors that lead to positive or negative outcomes in youth participants. This process of defining and validating data sources, employing data management software, and extracting information into visualizations that support the analysis is integral to data and knowledge management and applicable to any application of data analytics.`,
     link: "/e-portfolio/assets/data/Data_YRBSS_543.pdf",
   },
   {
